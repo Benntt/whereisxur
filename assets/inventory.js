04825e02d48303b93731c5cba24ae91b8f11a9ba
Damnitbennett <bennett@damnitbennett.com>
2025-10-25 16:33:33 -0400

    Update inventory.js

--- assets/inventory.js
+++ assets/inventory.js
@@ -3,7 +3,7 @@ async function loadXurInventory() {
   container.innerHTML = "<p>Loading Xûr’s inventory...</p>";
 
   try {
-    const response = await fetch("../data/xur_inventory_enriched.json");
+    const response = await fetch("/whereisxur/data/xur_inventory_enriched.json");
 
     const data = await response.json();
 
